d8b63a7b00836b9eac369393c5f77a75116a061e
bomber <tangyihong063@163.com>
2019-05-08 13:56:51 +0800

    用for循环来简化复杂代码

--- main.js
+++ main.js
@@ -1,39 +1,12 @@
 var img0=$('div img').eq(0)
-var img1=$('div img').eq(1)
-var img2=$('div img').eq(2)
-var img3=$('div img').eq(3)
-var imgs=$('div img')
-
-var button0=$('button').eq(0)
-var button1=$('button').eq(1)
-var button2=$('button').eq(2)
-var button3=$('button').eq(3)
 var buttons=$('button')
+var imgArray=['first','second','third','forth']
 
-button0.click(function() {
+for(let i=0;i<buttons.length;i++){
+  $('button').eq(i).click(function() {
   img0.removeClass()
-  img0.addClass('first')
+  img0.addClass(imgArray[i])
   buttons.removeClass('red')
-  button0.addClass('red')
+  $('button').eq(i).addClass('red')
 })
-
-button1.click(function() {
-  img0.removeClass()
-  img0.addClass('second')
-  buttons.removeClass('red')
-  button1.addClass('red')
-})
-
-button2.click(function() {
-  img0.removeClass()
-  img0.addClass('third')
-  buttons.removeClass('red')
-  button2.addClass('red')
-})
-
-button3.click(function() {
-  img0.removeClass()
-  img0.addClass('forth')
-  buttons.removeClass('red')
-  button3.addClass('red')
-})
+}
