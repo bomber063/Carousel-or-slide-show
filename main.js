e16595eb673d270abd85191c200cf0de8fb39949
bomber <tangyihong063@163.com>
2019-05-08 18:38:37 +0800

    用setInterval()使图片自动滚动

--- main.js
+++ main.js
@@ -10,3 +10,10 @@ for(let i=0;i<buttons.length;i++){
   $('button').eq(i).addClass('red')
 })
 }
+
+var p=-1
+  var timeId=setInterval(() => {
+    p++;
+    $('button').eq(p%4).trigger('click')//当setInterval里面的函数触发了tringger后，会接着触发click，也就是帮助人去点击操作
+    console.log(p,p%4)
+  }, 1000);
