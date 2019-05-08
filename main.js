f851fb486d377c0773dead3655f119a940a60c6b
bomber <tangyihong063@163.com>
2019-05-08 21:20:07 +0800

    实现鼠标滑进图片和按钮，图片保持不动，移除继续自动滚动

--- main.js
+++ main.js
@@ -17,3 +17,25 @@ var p=-1
     $('button').eq(p%4).trigger('click')//当setInterval里面的函数触发了tringger后，会接着触发click，也就是帮助人去点击操作
     console.log(p,p%4)
   }, 1000);
+
+  $('button').mouseenter(function() {
+    clearTimeout(timeId)
+  })
+  
+  $('button').mouseleave(function() {
+    timeId=setInterval(() => {
+      p++;
+      $('button').eq(p%4).trigger('click')
+    }, 1000);
+  })
+  
+  $('div').mouseenter(function() {
+    clearTimeout(timeId)
+  })
+  
+  $('div').mouseleave(function() {
+    timeId=setInterval(() => {
+      p++;
+      $('button').eq(p%4).trigger('click')
+    }, 1000);
+  })
