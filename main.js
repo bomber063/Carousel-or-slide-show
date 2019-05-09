467b2c91cc534f6998992583de226bfaf722885f
bomber <tangyihong063@163.com>
2019-05-09 21:52:24 +0800

    使用JQ的CSS的API，减少CSS文件代码，删除JS里面的CSS数组，滚动间隔时间调整为2秒

--- main.js
+++ main.js
@@ -1,42 +1,46 @@
-var img0=$('div img').eq(0)
-var buttons=$('button')
-var imgArray=['first','second','third','forth']
+var img0 = $('div img').eq(0)
+var buttons = $('button')
 
-for(let i=0;i<buttons.length;i++){
-  $('button').eq(i).click(function() {
-  img0.removeClass()
-  img0.addClass(imgArray[i])
-  buttons.removeClass('red')
-  $('button').eq(i).addClass('red')
-  p=i//增加该段代码，是为了在点击按钮后把p和i保持一致，不然p会一致自动加1，这样就有可能跳过一些图片或者不从点击的图片开始滚动
-})
+for (let i = 0; i < buttons.length; i++) {
+  $('button').eq(i).click(function () {
+    // img0.removeClass()
+    //     img0.css('margin-left', function() {
+    //       let size = i * -200 + 'px'
+    //       console.log(size)
+    //       return size
+    //     })
+    img0.css({ 'margin-left': i * -200 + 'px' })
+    buttons.removeClass('red')
+    $('button').eq(i).addClass('red')
+    p = i//增加该段代码，是为了在点击按钮后把p和i保持一致，不然p会一致自动加1，这样就有可能跳过一些图片或者不从点击的图片开始滚动
+  })
 }
 
-var p=-1
-  var timeId=setInterval(() => {
-    p++;
-    $('button').eq(p%4).trigger('click')//当setInterval里面的函数触发了tringger后，会接着触发click，也就是帮助人去点击操作
-    console.log(p,p%4)
-  }, 1000);
+var p = -1
+var timeId = setInterval(() => {
+  p++;
+  $('button').eq(p % 4).trigger('click')//当setInterval里面的函数触发了tringger后，会接着触发click，也就是帮助人去点击操作
+  console.log(p, p % 4)
+}, 2000);
 
-  $('button').mouseenter(function() {
-    clearTimeout(timeId)
-  })
-  
-  $('button').mouseleave(function() {
-    timeId=setInterval(() => {
-      p++;
-      $('button').eq(p%4).trigger('click')
-    }, 1000);
-  })
-  
-  $('div').mouseenter(function() {
-    clearTimeout(timeId)
-  })
-  
-  $('div').mouseleave(function() {
-    timeId=setInterval(() => {
-      p++;
-      $('button').eq(p%4).trigger('click')
-    }, 1000);
-  })
+$('button').mouseenter(function () {
+  clearTimeout(timeId)
+})
+
+$('button').mouseleave(function () {
+  timeId = setInterval(() => {
+    p++;
+    $('button').eq(p % 4).trigger('click')
+  }, 2000);
+})
+
+$('div').mouseenter(function () {
+  clearTimeout(timeId)
+})
+
+$('div').mouseleave(function () {
+  timeId = setInterval(() => {
+    p++;
+    $('button').eq(p % 4).trigger('click')
+  }, 2000);
+})
